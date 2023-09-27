ae775e355ef36c1af507862f593a0ab3484cb5ee
Danil Kabanov <nill2@yandex.ru>
2023-09-27 13:50:21 +0200

    fix: pylint fixes (#6)
    * Create python-package-conda.yml
    
    * Create python-app.yml
    
    * fix: matrix
    
    * fix: env
    
    * fix:  import os
    
    * fix: remove healthcheck for now
    
    * fix: tests fixed
    
    * fix: delete unused  tests
    
    * fix: test file naming fixed to the convention
    
    * fix: added pyftpdlib to deps
    
    * fix: update conda
    
    * fix: force install pyftpdlib
    
    * fix: remove pyftpdlib from deps
    
    * fix: imstall pymongo for pytests
    
    * fix: order of action and folder names fixed
    
    * fix: cleaning up
    
    * fix: tests with dynamic ftp server and port 2121
    
    * fix: timeout for tests
    
    * fix: ftp_host
    
    * fix: FTP_HOST everywhere
    
    * fix: socket
    
    * fix: timeouts
    
    * fix: skip the failing test for now
    
    * fix: skip test
    
    * feat: clean up
    
    * feat: added debug logging lvl
    
    * fix: file naming
    
    * fix: naming
    
    * feat: readme
    
    * fix: readme
    
    * fix: readme
    
    * fix: readme
    
    * fix: install pylint
    
    * fix: pylint
    
    * fix: pylint: disable
    
    * fix: pylint errors cleanup
    
    * fix: skip test

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+'''
+This it a config file that contains all the secrets for the app
+'''
+
 # config.py
 import os
 
 # FTP server configuration
-FTP_ROOT = "/ftp/"
+FTP_ROOT = os.path.expanduser("~") # old "~/ftp/"
 FTP_PORT = 2121
 FTP_HOST = os.getenv("FTP_HOST", "localhost")
 # change this to github secrets once move to production and use server deployments
@@ -10,7 +14,11 @@ FTP_USER = "user" # ${{ secrets.FTP_USER }}
 FTP_PASSWORD = "password" # ${{ secrets.FTP_PASSWORD }}
 
 # MongoDB configuration
-MONGO_HOST = "mongodb+srv://appUser:qovkm123@cluster0.qfjxdop.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp" # ${{ secrets.MONGO_HOST }}
+MONGO_HOST = (
+    "mongodb+srv://appUser:qovkm123@cluster0.qfjxdop.mongodb.net/"
+    "?retryWrites=true&w=majority&appName=AtlasApp"
+)
+# ${{ secrets.MONGO_HOST }}
 MONGO_PORT = 27017
 MONGO_DB = "nill-home"
 MONGO_COLLECTION = "nill-home-photos"
--- ftptomongo.py
+++ ftptomongo.py
@@ -1,97 +1,143 @@
+"""
+This script implements an FTP server that uploads files to a MongoDB database.
+"""
+
 import os
 import socket
-import tempfile
+from datetime import datetime, timedelta
+from pyftpdlib.servers import FTPServer
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
-from pymongo.server_api import ServerApi
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
-from config import FTP_ROOT, FTP_PORT, MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_COLLECTION, FTP_USER, FTP_PASSWORD, ERROR_LVL, FTP_HOST
-from pyftpdlib.servers import FTPServer
-from datetime import datetime, timedelta
-import time
-import bson
+from config import FTP_ROOT, FTP_PORT, MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_COLLECTION
+from config import FTP_USER, FTP_PASSWORD, ERROR_LVL, FTP_HOST
 
 
 def connect_to_mongodb():
+    """
+    Connects to a MongoDB instance and returns a collection object.
+
+    Returns:
+        pymongo.collection.Collection: A MongoDB collection object.
+            Returns None if the connection fails.
+    """
     try:
         client = MongoClient(MONGO_HOST, MONGO_PORT)
-        db = client[MONGO_DB]
-        collection = db[MONGO_COLLECTION]
+        my_mongo_db = client[MONGO_DB]
+        collection = my_mongo_db[MONGO_COLLECTION]
         if ERROR_LVL=="debug" :
-            print(f"Connected to MongoDB at {MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}/{MONGO_COLLECTION}")       
+            print(f"Connected to MongoDB: {MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}/{MONGO_COLLECTION}")
         return collection
-    except ConnectionFailure as e:
-        print(f"Failed to connect to MongoDB: {e}")
+    except ConnectionFailure as collection_error:
+        print(f"Failed to connect to MongoDB: {collection_error}")
         return None
 
 def db_cleanup(collection):
-        if collection is not None:  # Check if collection is not None
-            collection.delete_many({})
-            if ERROR_LVL=="debug" :
-                print(f"Deleted all documents from MongoDB")
-        else:
-            print("Failed to connect to MongoDB. File not uploaded.")
+    """
+    Cleans up the MongoDB collection by deleting all documents.
+
+    Args:
+        collection (pymongo.collection.Collection): The MongoDB collection to clean up.
+
+    Returns:
+        None
+    """
+    if collection is not None:  # Check if collection is not None
+        collection.delete_many({})
+        if ERROR_LVL=="debug" :
+            print("Deleted all documents from MongoDB")
+    else:
+        print("Failed to connect to MongoDB. File not uploaded.")
 
 def delete_expired_data(collection, field_name, expiration_period_days):
+    """
+    Deletes documents from the collection that are older than a specified expiration period.
+
+    Args:
+        collection (pymongo.collection.Collection): The MongoDB collection to delete documents from.
+        field_name (str): The name of the field that stores the expiration date in the documents.
+        expiration_period_days (int): The number of days that define the expiration period.
+
+    Returns:
+        int: The number of documents deleted.
+    """
     # Calculate the expiration date as a datetime object
     expiration_date = datetime.utcnow() - timedelta(days=expiration_period_days)
-    
     # Create a filter to find documents older than the expiration date
-    filter = {field_name: {"$lt": expiration_date}}
-    
+    del_filter = {field_name: {"$lt": expiration_date}}
     # Delete the expired documents and get the count of deleted documents
-    result = collection.delete_many(filter)
-    
+    result = collection.delete_many(del_filter)
     return result.deleted_count
 
 
 class MyHandler(FTPHandler):
-    def on_file_received(self, file_path):
+    """
+    Custom FTPHandler for handling FTP server operations.
+
+    This class extends FTPHandler to provide custom functionality for handling FTP server
+    operations such as uploading files to MongoDB and cleaning up expired documents.
+
+    Attributes:
+        authorizer: The authorizer for user authentication.
+    """
+    def on_file_received(self, received_file): # pylint: disable=arguments-renamed
         # Upload the received file to MongoDB
         collection = connect_to_mongodb()
         if collection is not None:  # Check if collection is not None
-            with open(file_path, "rb") as file:
+            with open(received_file, "rb") as file:
                 timestamp = datetime.now().timestamp()
                 file_data = file.read()
-                bsonTime = bson.Int64(timestamp * 1000)
-                collection.insert_one({"filename": os.path.basename(file_path), "data": file_data, "size": os.path.getsize(file_path), "date": timestamp, "bsonTime": datetime.now()})
-                if ERROR_LVL=="debug" :
-                    print(f"Uploaded {os.path.basename(file_path)} to MongoDB")
-            
+                collection.insert_one({
+                "filename": os.path.basename(received_file),
+                "data": file_data,
+                "size": os.path.getsize(received_file),
+                "date": timestamp,
+                "bsonTime": datetime.now()
+                })
+                #if ERROR_LVL=="debug" :
+                #   print("Uploaded"+file+"to MongoDB")# {os.path.basename(file)}
+
             # Clean up the expired documents in the database
             expired_docs_deleted = delete_expired_data(collection, "date", 365)
-            print(f"Deleted " + expired_docs_deleted + " documents")
-                           
+            print("Deleted " + str(expired_docs_deleted) + " documents")
             # Delete the file from the FTP server
-            file_to_del = os.path.join(FTP_ROOT, file_path)
+            file_to_del = os.path.join(FTP_ROOT, received_file)
             os.remove(file_to_del)
             if ERROR_LVL=="debug" :
-                print(f"deleted {os.path.basename(file_path)} from the FTP server")
+                print(f"deleted {os.path.basename(received_file)} from the FTP server")
         else:
             print("Failed to connect to MongoDB. File not uploaded.")
-            
-    
 
 def run_ftp_server():
+    """
+    Start and run the FTP server.
+
+    This function starts the FTP server and listens for incoming connections.
+    It performs the necessary setup and configuration for the server.
+
+    Returns:
+        None
+    """
     authorizer = DummyAuthorizer()
     authorizer.add_user(FTP_USER, FTP_PASSWORD, FTP_ROOT, perm="elradfmw")
-    
+
+    print("My: Starting FTP server..." + FTP_ROOT+ "\n")
+
     handler = MyHandler
     handler.authorizer = authorizer
-    
+
     # Explicitly bind the socket to the desired host and port
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((FTP_HOST, FTP_PORT))  # Adjust host and port as needed
     server_socket.listen(5)  # Start listening for incoming connections
-    
+
     server = FTPServer(server_socket, handler)
     server.serve_forever()
 
 if __name__ == "__main__":
     if not os.path.exists(FTP_ROOT):
         os.makedirs(FTP_ROOT)
-    
     try:
         run_ftp_server()
     except KeyboardInterrupt:
