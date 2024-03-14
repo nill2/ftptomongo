a5c98a7d16bec948675181b57df25f9e9fc8e910
Danil Kabanov <nill2@yandex.ru>
2024-03-14 21:21:51 +0100

    fix: otlp metrics

--- __main__.py
+++ __main__.py
@@ -1,17 +1,29 @@
+'''
+This is a main file to start it all
+'''
 import os
 import sys
-import signal
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics import Counter, MeterProvider
-from opentelemetry.exporter.prometheus import PrometheusMetricsExporter
-import ftptomongo
+import signal  # Import the signal module without using it directly
 
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+
+import ftptomongo
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 
-def signal_handler(sig, frame):  
+def signal_handler(sig, frame):  # pylint: disable=unused-argument
     """
-    Signal handler  function to handle KeyboardInterrupt (Ctrl+C).
+    Signal handler function to handle KeyboardInterrupt (Ctrl+C).
     """
     print("Ctrl+C detected. Exiting...")
     sys.exit(0)
@@ -23,29 +35,29 @@ def main():
     """
     print("Executing ftptomongo package as a script")
 
-    # Set up OpenTelemetry metrics
-    meter_provider = MeterProvider()
-    meter = meter_provider.get_meter(__name__)
-    exporter = PrometheusMetricsExporter(endpoint="http://localhost:9090")
-    meter_provider.start(meter, exporter)
+    # setup OTLP
+    # Service name is required for most backends
+    resource = Resource(attributes={
+                        SERVICE_NAME: "ftptomongo"
+                        })
 
-    # Define metrics
-    requests_counter = meter.create_metric(
-        "requests",
-        "Number of requests",
-        "requests",
-        Counter,
-        int,
+    trace_provider = TracerProvider(resource=resource)
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="<traces-endpoint>/v1/traces"))
+    trace_provider.add_span_processor(processor)
+    trace.set_tracer_provider(trace_provider)
+
+    reader = PeriodicExportingMetricReader(
+        OTLPMetricExporter(endpoint="<traces-endpoint>/v1/metrics")
     )
+    meter_provider = MeterProvider(resource=resource, metric_readers=[reader])
+    metrics.set_meter_provider(meter_provider)
 
     # Set the signal handler for SIGINT (Ctrl+C)
     signal.signal(signal.SIGINT, signal_handler)
 
     # Your main application logic goes here
-    while True:
-        ftptomongo.run_ftp_server()
-        # Increment requests counter
-        requests_counter.add(1)
+    ftptomongo.run_ftp_server()
+
 
 if __name__ == "__main__":
     main()
